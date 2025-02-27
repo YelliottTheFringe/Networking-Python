37224ca295a9550985900c876fd2ac9a90875eed
YelliottTheFringe <123002260+YelliottTheFringe@users.noreply.github.com>
2025-02-26 21:07:34 -0600

    Update ping_hosts.py
    Annotated and fixed

--- ping_hosts.py
+++ ping_hosts.py
@@ -3,20 +3,20 @@ import subprocess
 alive=[]
 
 def get_ip_addresses():
-    prefix = '192.168.9.'
+    prefix = '192.168.9.' # Change this file to current network
     ip_addresses = []
-    for i in range(10, 300):
+    for i in range(4, 300): #avoid the first four devices, they are networking devices.
         ip_address = prefix + str(i)
         ip_addresses.append(ip_address)
     return ip_addresses
 
 def get_alive_status(host):
     ALIVE = False
-    command = 'ping ' + host + ' -n 1 -w 500'
+    command = 'ping ' + host + ' -n 1 -w 500' # pings the dang thing
 
-    p = subprocess.Popen(command, stdout=subprocess.PIPE)
-    output = p.stdout.read().decode()
-    if 'TTL=' in output:
+    p = subprocess.Popen(command, stdout=subprocess.PIPE) # uses subprocess to process the ping
+    output = p.stdout.read().decode() # OUTPUT!
+    if 'TTL=' in output: # TTL tells you it is a device and it is alive (and what device)
         ALIVE = True
         print('ALIVE')
     return ALIVE
